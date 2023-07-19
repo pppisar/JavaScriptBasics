fd72d49273a5eed029c6f4a17969a7fc701b7e7c
Oleksii Pysarenko <pysarole@gmail.com>
2023-07-19 20:13:16 +0200

    Basic syntax and working with html document

---
+++ 01-Base/app.js
@@ -0,0 +1,113 @@
+// Переменньіе и базовьіе операции
+// Переменная - let | константа const
+let num = 12; // number
+console.log("Your number is " + num.toString());
+console.log(13, '+', 12, '=', 13 + 12);
+
+let surname = 'Pysarenko'; // string
+const firstName = 'Oleksii';
+
+const isJavaScript = true; // bool
+
+// alert блокирует загрузку страницьі
+// alert("You are is " + surname + ' ' + firstName);
+
+// surname = 'Samarsky' // ok - изменяем let
+// firstName = 'Ivan' // error - Изменение const
+
+/** Possible names for variables
+ * let $ = 'test'
+ * let $name = 1337 
+*/
+
+const fullName = firstName + ' ' + surname;
+
+
+// ------------------------------------------
+// Работа с приложением
+
+const $input1 = document.getElementById('input1')
+const $input2 = document.getElementById('input2')
+
+const $operationBtns = document.getElementsByClassName('flex-grow-1')
+
+const $plusBtn = document.getElementById('plus')
+const $minusBtn = document.getElementById('minus')
+const $mulBtn = document.getElementById('mul')
+const $divBtn = document.getElementById('div')
+
+const $button = document.getElementById('submit')
+const $result = document.getElementById('result')
+
+// operations: +, -, *, /
+let operation = '+'
+
+function chooseOperatore(button) {
+    for (let i = 0; i < $operationBtns.length; i++) {
+        if ($operationBtns[i] == button) {
+            $operationBtns[i].classList.remove('btn-danger')
+            $operationBtns[i].classList.add('btn-success')
+        }
+        else {
+            $operationBtns[i].classList.remove('btn-success')
+            $operationBtns[i].classList.add('btn-danger')
+        }
+    }
+}
+
+$plusBtn.onclick = function() {
+    operation = '+'
+    chooseOperatore($plusBtn)
+}
+
+$minusBtn.onclick = function () {
+    operation = '-'
+    chooseOperatore($minusBtn)
+}
+
+$mulBtn.onclick = function () {
+    operation = '*'
+    chooseOperatore($mulBtn)
+}
+
+$divBtn.onclick = function () {
+    operation = '/'
+    chooseOperatore($divBtn)
+}
+
+function calculateOperation(inp1, inp2, op) {
+    const val1 = Number(inp1.value)
+    const val2 = Number(inp2.value)
+    switch (op) {
+        case '+':
+            return val1 + val2
+            break
+        case '-':
+            return val1 - val2
+            break
+        case '*':
+            return val1 * val2
+            break
+        case '/':
+            return val1 / val2
+            break
+        default:
+            console.log(`Invalid operation: ${op}`)
+    }
+}
+
+$button.onclick = function () {
+    $result.textContent = calculateOperation($input1, $input2, operation)
+}
+
+// Тернарньіе вьіражения:
+// function plusMinus(num1, num2, op) {
+//         if op == '+':         else:
+//    return op == '+' ? num1 + num2 : num1 - num2
+// }
+// 
+
+
+
+
+
