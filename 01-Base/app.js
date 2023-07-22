a4f9807932985e76a4426bf81d2e345e6880d4a0
Oleksii Pysarenko <pysarole@gmail.com>
2023-07-22 10:37:31 +0200

    Debugging

--- 01-Base/app.js
+++ 01-Base/app.js
@@ -1,5 +1,5 @@
-// Переменньіе и базовьіе операции
-// Переменная - let | константа const
+// Variable and basic operations
+// Variable - let | constant - const
 let num = 12; // number
 console.log("Your number is " + num.toString());
 console.log(13, '+', 12, '=', 13 + 12);
@@ -9,11 +9,11 @@ const firstName = 'Oleksii';
 
 const isJavaScript = true; // bool
 
-// alert блокирует загрузку страницьі
+// alert blocks page loading
 // alert("You are is " + surname + ' ' + firstName);
 
-// surname = 'Samarsky' // ok - изменяем let
-// firstName = 'Ivan' // error - Изменение const
+// surname = 'Samarsky' // ok - modify let
+// firstName = 'Ivan' // error - modify const
 
 /** Possible names for variables
  * let $ = 'test'
@@ -24,7 +24,7 @@ const fullName = firstName + ' ' + surname;
 
 
 // ------------------------------------------
-// Работа с приложением
+// Working with the html
 
 const $input1 = document.getElementById('input1')
 const $input2 = document.getElementById('input2')
@@ -78,6 +78,16 @@ $divBtn.onclick = function () {
 function calculateOperation(inp1, inp2, op) {
     const val1 = Number(inp1.value)
     const val2 = Number(inp2.value)
+
+    /* Debugging in browser
+     * Using developer tools in the Source tab
+     * You can check values by hovering over variables
+     * You can also execute the code step by step
+     * You can also put your own breakpoints by 
+     * clicking on the line number in the browser
+     */
+    // debugger
+
     switch (op) {
         case '+':
             return val1 + val2
@@ -100,7 +110,7 @@ $button.onclick = function () {
     $result.textContent = calculateOperation($input1, $input2, operation)
 }
 
-// Тернарньіе вьіражения:
+// Ternary conditional operator:
 // function plusMinus(num1, num2, op) {
 //         if op == '+':         else:
 //    return op == '+' ? num1 + num2 : num1 - num2
